46a42511249e9518ce298c576c186787d1c7e520
Lucimar Almeida <79388441+lucimar-almeida@users.noreply.github.com>
2023-02-12 19:18:47 -0400

    retorno

---
+++ Estudos/Javascript/Funções/script.js
@@ -0,0 +1,5 @@
+alert("Olá, seja bem vindo")
+let nome= prompt("Qual é o seu nome?")
+console.log("ola, " + nome +"!")
+
+document.write("<h1>Bem vindo ao nosso projeto fabrica de aplicativo " + nome + "</h1>")
