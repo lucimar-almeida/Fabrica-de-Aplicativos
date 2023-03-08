56b766f6ed7e77b52f7b031ad8e24fa13845cea2
Lucimar Almeida <79388441+lucimar-almeida@users.noreply.github.com>
2023-03-08 14:22:37 -0400

    atualizzações

--- Projetos/Estudos/estudo3/index.js
+++ Projetos/Estudos/estudo3/index.js
@@ -1,9 +1,15 @@
-/**
- * @format
- */
+import React from 'react'
+import { View, Text } from 'react-native'
+import { Component } from 'react'
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+function App(){
 
-AppRegistry.registerComponent(appName, () => App);
+    return(
+        <View>
+            <Text>meu primeiro app!</Text>
+        </View>
+    )
+
+}
+
+export default App
