a3b703e86aee404ac335e6d07fd66ac7151d95e0
Lucimar Almeida <79388441+lucimar-almeida@users.noreply.github.com>
2023-03-09 13:20:12 -0400

    Expo

---
+++ Projetos/Estudos/estudo1/App.js
@@ -0,0 +1,12 @@
+import react from "react"
+import { View, Text } from "react-native"
+
+export default function app(){
+
+  return(
+    <View>
+      <Text>meu primeiro app</Text>
+    </View>
+  )
+
+}
