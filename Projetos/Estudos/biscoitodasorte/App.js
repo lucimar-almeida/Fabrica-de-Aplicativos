06af364a52f0cb6da40c2410690f6b34a50b18f9
Lucimar Almeida <79388441+lucimar-almeida@users.noreply.github.com>
2023-03-15 10:49:04 -0400

    App do biscoito

--- Projetos/Estudos/biscoitodasorte/App.js
+++ Projetos/Estudos/biscoitodasorte/App.js
@@ -1,18 +1,32 @@
-import React from "react";
-import { View, Text, TouchableOpacity, } from "react-native"
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity, StyleSheet, Image } from "react-native"
 
 function App(){
+  const [img, setImg] = useState(require('./src/biscoito.png'))
 
   return(
-    <View>
+    <View style={styles.container}>
 
-      <Text>Ola mundo!</Text>
+    
+      <Image source={ img }
+        style={styles.img}
+      />
 
+      <View style={styles.caixaFrase}>
+        <Text style={styles.frase}> " Texto do biscoito "</Text>
+      </View>
 
+      <TouchableOpacity style={styles.btn}>
+        <View style={styles.btnArea}>
+          <Text style={styles.btnFrase}>Quebrar o biscoito!</Text>
+        </View>
+      </TouchableOpacity>
 
-
-
-
+      <TouchableOpacity style={[styles.btn, { marginTop: 20, borderColor: 'black'}]}>
+        <View style={[styles.btnArea, {}]}>
+          <Text style={[styles.btnFrase, {color: 'black'}]}>Quebrar o biscoito!</Text>
+        </View>
+      </TouchableOpacity>
 
 
 
@@ -22,5 +36,55 @@ function App(){
 
 
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    textAlign: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  img:{
+    width: 230,
+    height: 230,
+  },
+
+  caixaFrase:{
+    margin: 20,
+
+  },
+
+  frase:{
+    fontSize: 20,
+    fontStyle: 'italic',
+    color: '#dd7b22',
+    marginTop: 30,
+    marginBottom: 30,
+
+  },
+
+  btn:{
+    borderRadius: 25,
+    borderWidth: 2,
+    borderColor: '#dd7b22',
+
+  },
+
+  btnArea:{
+    padding: 14,
+    justifyContent: 'center',
+    alignItems: "center",
+    
+    
+  },
+
+  btnFrase:{
+    fontSize: 15,
+    color: '#dd7b22',
+    
+  },
+
+})
 
 export default App
