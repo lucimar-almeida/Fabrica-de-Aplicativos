05eaa228f97c36606fb9b30955b7ead38ccded34
Lucimar Almeida <79388441+lucimar-almeida@users.noreply.github.com>
2023-03-20 11:55:53 -0400

    New app

---
+++ Projetos/Estudos/biscoitodasorte/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import {Text, View, StyleSheet} from "react"
+
+function App(){
+
+  const styles = style.StyleSheet()
+
+
+  <View>
+    <Text>Meu App da sorte, </Text>
+  
+  
+  
+  
+  
+  
+  
+  
+  </View>
+
+}
+
+export default App
