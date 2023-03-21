02d470a6c825940f15481f3317f9930e1e0e81dd
Lucimar Almeida <79388441+lucimar-almeida@users.noreply.github.com>
2023-03-21 18:43:14 -0400

    app

--- Projetos/Estudos/biscoitodasorte/App.js
+++ Projetos/Estudos/biscoitodasorte/App.js
@@ -1,23 +1,90 @@
-import React from "react";
-import {Text, View, StyleSheet} from "react"
+import React, { useState } from "react";
+import {Text, View, StyleSheet, Image, TouchableOpacity} from "react-native"
 
 function App(){
 
-  const styles = style.StyleSheet()
+  const [img, setImg] = useState(require('./src/biscoito.png'))
 
+  return(
+  
+    <View style={styles.container}>
 
-  <View>
-    <Text>Meu App da sorte, </Text>
+      <Image style={styles.img}
+      source={img}
+      />
+
+      <Text style={styles.frase}>" Esta é a minha primeira frase! "</Text>
+
+      <TouchableOpacity style={styles.button} onPress={()=> alert("test")}>
+        <View style={styles.btn}>
+          <Text style={styles.textBtn}>Quebrar Biscoito</Text>
+        </View>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={[styles.button, {marginTop: 20, borderColor: 'white'}]} onPress={()=> alert("test")}>
+        <View style={styles.btn}>
+          <Text style={[styles.textBtn, {color: 'white'}]}>Reiniciar Biscoito</Text>
+        </View>
+      </TouchableOpacity>
+    
+    
+    </View>
   
-  
-  
-  
-  
-  
-  
-  
-  </View>
+)
 
 }
+
+const styles = StyleSheet.create({
+
+  container:{
+    flex: 1,
+    backgroundColor: 'black',
+    color: 'white',
+    justifyContent: 'center',
+    textAlign: 'center',
+    alignItems: 'center',
+  },
+
+  frase:{
+    color: '#dd7b22',
+    fontSize: 20,
+    fontStyle: 'italic',
+    margin: 30,
+    textAlign: 'center'
+
+
+  },
+
+  img: {
+    width: 250,
+    height: 250,
+  },
+
+  button: {
+    borderColor: '#dd7b22',
+    borderWidth: 3,
+    width: 230,
+    height: 50,
+    borderRadius: 25,
+  
+  },
+
+  btn: {
+    color: '#dd7b22',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+
+  },
+
+  textBtn: {
+    color: '#dd7b22',
+    fontSize: 18,
+    fontWeight: 'bold',
+  
+  }
+
+
+})
 
 export default App
