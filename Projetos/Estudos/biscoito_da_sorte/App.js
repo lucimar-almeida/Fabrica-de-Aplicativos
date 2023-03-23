d2ac573f6e3052ae3aacdd44b569eeca69738281
Lucimar Almeida <79388441+lucimar-almeida@users.noreply.github.com>
2023-03-23 16:44:55 -0400

    app biscoito

--- Projetos/Estudos/biscoito_da_sorte/App.js
+++ Projetos/Estudos/biscoito_da_sorte/App.js
@@ -4,7 +4,33 @@ import { View, Text, Image, TouchableOpacity, StyleSheet } from "react-native"
 function App(){
 
   const [img, setImg] = useState(require('./src/biscoito.png'))
+  const [textoFrase, setTextofrase] = useState('')
 
+  let frases = [
+    'Siga os bons e aprenda com eles',
+    'O bom-senso vale mais do que muito conhecimento',
+    'O riso é a menor distancia entre duas pessoas',
+    'deixe de lado  as preocupações e seja feliz',
+    'Realize o obvio, pense no improvavel e conquiste o impossivel',
+    'Acredite em milagres, mas não dependa deles',
+    'A maior barreira para o sucesso é o medo do fracasso'
+  ]
+
+function quebraBiscoito(){
+  let numeroAleatorio = Math.floor(Math.random() * frases.length)
+  //let fraseSorteada = frases[numeroAleatorio]
+  let fraseSorteada = (frases[numeroAleatorio])
+
+  setTextofrase(' " ' +  fraseSorteada + ' " ')
+
+  setImg(require('./src/biscoito2.png'))
+}
+
+function reiniciar(){
+  setTextofrase('')
+  setImg(require('./src/biscoito.png'))
+
+}
 
   return(
 
@@ -12,11 +38,19 @@ function App(){
 
       <Image style={styles.img} source={img}/>
 
-      <Text style={styles.frase}>" Minha primeira frase em React Native! "</Text>
+      <Text style={styles.frase}> { textoFrase } </Text>
 
+      <TouchableOpacity style={[styles.botao, {marginTop: 50}]} onPress={ quebraBiscoito}>
+        <View style={styles.areaBtn}>
+          <Text style={styles.fraseBtn}>Quebrar Biscoito</Text>
+        </View>
+      </TouchableOpacity>
 
-
-
+      <TouchableOpacity style={[styles.botao, {borderColor: 'white'}]} onPress={ reiniciar }>
+        <View style={[styles.areaBtn]}>
+          <Text style={[styles.fraseBtn, {color: 'white'}]}>Reiniciar Biscoito</Text>
+        </View>
+      </TouchableOpacity>
 
 
 
@@ -34,19 +68,44 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: 'black'
   },
 
   frase:{
     color: '#dd7b22',
-    fontSize: 17,
+    fontSize: 20,
     fontStyle: 'italic',
     textAlign: 'center',
+    padding: 5,
+    marginTop: 30,
   },
 
   img:{
     width: 250,
     height: 250,
 
+  },
+
+  botao: {
+    borderWidth: 2,
+    borderColor: '#dd7b22',
+    borderRadius: 25,
+    width: 200,
+    height: 50,
+    marginTop: 20,
+  },
+
+  areaBtn:{
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+
+  },
+
+  fraseBtn:{
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#dd7b22',
   }
 
 
