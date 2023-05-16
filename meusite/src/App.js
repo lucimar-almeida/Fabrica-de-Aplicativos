2508a992acfd29bd3ef64d41f5e4ed616cbe5ce6
Lucimar Almeida <79388441+lucimar-almeida@users.noreply.github.com>
2023-05-16 18:21:26 -0400

    inicio do reactJS

---
+++ meusite/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Bemvindo = () => {
+    return(
+
+        <div>
+            <h2>Bem-vindo</h2>
+        </div>
+    )
+}
+
+function App(){
+    return(
+
+        <di>
+            olá mundo
+            <Bemvindo/>
+            <h1>Ola mundo!</h1>
+        </di>
+
+
+
+
+
+
+
+    )
+
+}
+
+export default App
---
+++ meusite/src/social.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+function Social(){
+    return <h1>Social</h1>
+
+}
+
+export default Social
