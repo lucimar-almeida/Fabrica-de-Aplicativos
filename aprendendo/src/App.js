b0765b7da5f98b7438796c6cb2f3df7bd31e70b8
Lucimar Almeida <79388441+lucimar-almeida@users.noreply.github.com>
2023-05-28 17:44:56 -0400

    estudo de Hooks

--- aprendendo/src/App.js
+++ aprendendo/src/App.js
@@ -1,36 +1,35 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo, useCallback } from "react";
 
 function App() {
 
   const [input, setInput] = useState('')
 
-  const [tarefas, setTarefas] = useState([
-    'Pagar conta de luz',
-    'estudar react Hooks',
-    'Trabalhar na usina'
-  ])
+  const [tarefas, setTarefas] = useState([])
 
-  function handleAdd(){
+  const handleAdd = useCallback(()=> {
+
+    if(input !== ''){
+
     setTarefas([...tarefas, input])
     setInput('')
-  }
+    }else{
+      alert("Digite algo!!!")
+    }
+  }, [input, tarefas])
 
-  useEffect(()=> {
 
-    localStorage.setItem('tarefas', JSON.stringify(tarefas))
+  useEffect(()=>{
+    localStorage.setItem("tarefas", JSON.stringify('tarefas'))
 
-  }, [tarefas])
+  },[tarefas])
 
-  useEffect(()=> {
-
-    localStorage.getItem('tarefas', JSON.parse('tarefas'))
-
-  }, [tarefas])
+const totalTarefas = useMemo(()=> tarefas.length, [tarefas])
 
   return (
     <div>
       <ul>
-          {tarefas.map(tarefa => (<li key={tarefa}>{tarefa}</li>))}
+          {tarefas.map(tarefa => (<li key={tarefa}>{tarefa}</li>))}<br/>
+          <strong><text>Voce tem {totalTarefas} tarefas!</text></strong><br/>
           <input type="text" value={input} onChange={e => setInput(e.target.value)}/>
           <button type="button" onClick={handleAdd}>Adicionar tarefa</button>
       </ul>
