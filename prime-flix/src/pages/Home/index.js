6c6ef8125e2601844525f94a4084287d56a87d42
Lucimar Almeida <79388441+lucimar-almeida@users.noreply.github.com>
2023-07-17 19:23:05 -0400

    filmes

--- prime-flix/src/pages/Home/index.js
+++ prime-flix/src/pages/Home/index.js
@@ -1,8 +1,45 @@
 
+import { useEffect, useState } from "react";
+import api from '../../services/api'
+
+// URL da API: /movie/now_playing?api_key=3790253604aa07af218badb6767b5e8e&language=pt-BR
+
 function Home(){
+
+  const [filmes, setFilmes] = useState([])
+
+  useEffect(()=>{
+
+    async function loadFilmes(){
+      const response = await api.get("movie/now_playing",{
+        params:{
+          api_key: "3790253604aa07af218badb6767b5e8e",
+          language: "pt-BR",
+          page: 1,
+        }
+      })
+
+      //console.log(response.data.results.slice(0,10))
+
+      setFilmes(response.data.results.slice(0,10))
+    }
+
+    loadFilmes()
+
+  },[])
+
   return(
-    <div>
-      <h1>BEM VINDO A HOME</h1>
+    <div className="container">
+      <div className="lista-filmes">
+        {filmes.map((filme)=>{
+          return(
+            <article key={filme.id}>
+              <strong>{filme.title}</strong>
+              <img src={`htts://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filmes.title}/>
+            </article>
+          )
+        })}
+      </div>
     </div>
   )
 }
---
+++ prime-flix/src/services/api.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+
+// Base da URL: https://api.themoviedb.org/3/
+// URL da API: /movie/now_playing?api_key=3790253604aa07af218badb6767b5e8e&language=pt-BR
+
+const api = axios.create({
+    baseURL: 'https://api.themoviedb.org/3/'
+})
+
+export default api
